b6d8e7c0ffba9911fc72dd0c60bc5d6ecb8be12d
Ayham Orfali <orfali.ayham@gmail.com>
2017-08-02 13:58:51 +0300

    sqlite3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'application/hello'
+  #root 'application#hello'
+
+
 end
